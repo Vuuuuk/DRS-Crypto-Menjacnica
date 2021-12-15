f28ebb4336bf26698af2900697614935252a3644
dule333 <radulovicdusan99@yahoo.com>
2021-12-15 15:26:31 +0100

    refactor: moved json conversion into ExchangeRateAPI.py

--- ExchangeRateAPI.py
+++ ExchangeRateAPI.py
@@ -1,7 +1,10 @@
+import json
+
 import requests
 
 
 def getExchangeRates():
     url = 'https://rest.coinapi.io/v1/exchangerate/USD?invert=true'
     headers = {'X-CoinAPI-Key': 'E72E3DA0-51EB-42C1-9B66-00ADA4D114E3'}
-    return requests.get(url, headers=headers)
+    parsed = json.loads(requests.get(url, headers=headers).content)
+    return json.dumps(parsed, indent=4, sort_keys=True)
