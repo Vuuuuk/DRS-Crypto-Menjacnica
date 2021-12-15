6868fc87a2f02357853a444ffaef3352d049eee0
Vuk Radunović <el.teh.it.radunovic.vuk@gmail.com>
2021-12-15 14:38:32 +0100

    feat: MongoAPI using remote MongoDB instance

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ from ExchangeRateAPI import getExchangeRates
 exchangeRateJSON = getExchangeRates()
 parsed = json.loads(exchangeRateJSON.content)
 print(json.dumps(parsed, indent=4, sort_keys=True))
+
